b0e069d7dc4f5727289a4080abc97e6fb4481619
Santiib <santiagobozzo@hotmail.com>
2016-11-18 00:20:42 -0300

    Update deps.mk

--- deps.mk
+++ deps.mk
@@ -1,5 +1,5 @@
 abb: abb.o pila.o
 heap: heap.o
 comm: comm.o hash.o
-#uniq-count: uniq-count.o hash.o
-#heap-utils.c: heap-utils.o heap.o
+uniq-count: uniq-count.o hash.o
+heap-utils.c: heap-utils.o heap.o #no se si la linea 2(heap) haga falta o no pero la dejo por las dudas porque es parte del tp.
